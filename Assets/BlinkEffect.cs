0ffc49f1dc6675c82acca2d4a1cc18105e0ce7a6
CptDads <tjj.jonker@gmail.com>
2021-10-31 17:54:51 +0100

    Added a blinking effect for TMPro text

---
+++ Assets/BlinkEffect.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using TMPro;
+
+public class BlinkEffect : MonoBehaviour
+{
+    [SerializeField] private Color minColor;
+    [SerializeField] private Color maxColor;
+
+    [SerializeField] private float speed;
+
+    private TextMeshProUGUI tmpro;
+    private float sinX;
+
+    private void Awake() => tmpro = GetComponent<TextMeshProUGUI>();
+
+    private void Update()
+    {
+        var Delta = minColor - maxColor;
+
+        var color = Mathf.Sin(sinX) * (Delta / 2) + minColor - (Delta / 2);
+        tmpro.color = color;
+
+        sinX += speed * Time.deltaTime;
+    }
+}
